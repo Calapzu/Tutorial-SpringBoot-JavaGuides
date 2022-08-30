82d9f7f91587f5bf0221dfc13b634c60c280ce4f
Calapzu <yamsoncalapzu@gmail.com>
2022-08-29 23:24:56 -0500

    Se trabaha sobre la URL

--- springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
+++ springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
@@ -1,6 +1,8 @@
 package com.springboot.first.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,5 +24,21 @@ public class StudentController {
         students.add(new Student("Jhoan", "Calapzu"));
         students.add(new Student("Yamson", "Jhoan"));
         return students;
+    }
+
+    //http://localhost:8080/student/yamson/calapzu
+    //@PathVariable annotation
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student studentpathVariable(@PathVariable("firstName") String firstName,
+                                       @PathVariable("lastName") String lastName){
+        return new Student(firstName, lastName);
+    }
+
+    //Crea una API Rest para manejar los parametros
+    //http://localhost:8080/student?firstName=Yamson&lastName=Calapzu
+    @GetMapping("/student/query")
+    public Student studentQueryParam(@RequestParam(name = "firstName") String firstName,
+                                     @RequestParam(name = "lastName") String lastName){
+        return new Student(firstName, lastName);
     }
 }
